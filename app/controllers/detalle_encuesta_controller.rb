e6aead992bec3ff85b7345c619144a69d59ae884
Jose De La Roca <jmdelaroca@Joses-MacBook-Pro-2.local>
2014-10-30 17:36:46 -0600

    Arreglos en detalle_encuesta

--- app/controllers/detalle_encuesta_controller.rb
+++ app/controllers/detalle_encuesta_controller.rb
@@ -4,21 +4,28 @@ class DetalleEncuestaController < ApplicationController
   # POST /detalle_encuesta.json
   def create
 
+  	# validar parametros
+  	detalle_encuesta_params
+
   	# obtener la encuesta y la pregunta
   	@detalle = DetalleEncuesta.where('encuesta_id = ? AND pregunta_id = ?', params[:encuesta_id], params[:pregunta_id])
 
-  	if @detalle
- 		@detalle.first.respuesta = Respuesta.find(params[:respuesta_id])
- 	end
-
   	respond_to do |format|
-      if @detalle.first.save
-        format.html { redirect_to @detalle.first, notice: 'Encuesta was successfully updated.' }
-        format.json { render json: @detalle.first, status: :ok }
-      else
-        format.html { render :edit }
-        format.json { render json: @detalle.first.errors, status: :unprocessable_entity }
-      end
+
+  		# validar que se hayan obtenido datos
+  		if @detalle.count > 0
+  			# asociar la respuesta 
+ 			@detalle.first.respuesta = Respuesta.find(params[:respuesta_id])
+ 	
+	      if @detalle.first.save
+	        format.json { render json: @detalle.first, status: :ok }
+	      else
+	        format.json { render json: @detalle.first.errors, status: :unprocessable_entity }
+	      end
+    	else
+    		# reportar error
+ 			format.json { render json: @detalle, status: :unprocessable_entity }
+ 		end
     end
   end
 
@@ -28,7 +35,8 @@ class DetalleEncuestaController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def detalle_encuesta_params
       params.require(:pregunta_id)
-      params.require(:encuesta_id).permit(:encuesta_id,:pregunta_id,:respuesta_id)
+      params.require(:encuesta_id)
+      params.permit(:encuesta_id,:pregunta_id,:respuesta_id)
       
     end
 end
